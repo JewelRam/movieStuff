603747bd7c0f00f637d60db1c8cfc0203937f684
Jewel Ramirez <chimpy4u@yahoo.com>
2018-07-10 15:32:59 -0700

    tried to make it work

---
+++ src/scripts/movieInput.js
@@ -0,0 +1,4 @@
+//get values from inputs
+const movieInput = document.querySelector("#movie").value
+const plotInput = document.querySelector("#plot").value
+const durationInput = document.querySelector("#duration").value
